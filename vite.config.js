741d0c894e1bbd8091857900d5eea2dd0102d3bf
senapati484 <sayansenapati2544@gmail.com>
2024-09-14 10:14:35 +0530

    adding it to progressive web application

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,51 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        short_name: "ToDoApp",
+        name: "ToDo List Application",
+        description: "A to-do list application",
+        background_color: "#ffffff",
+        theme_color: "#000000",
+        display: "standalone",
+        start_url: ".",
+        orientation: "portrait",
+        icons: [
+          {
+            src: "/favicon-16x16.png",
+            sizes: "16x16",
+            type: "image/png",
+          },
+          {
+            src: "/favicon-32x32.png",
+            sizes: "32x32",
+            type: "image/png",
+          },
+          {
+            src: "/apple-touch-icon.png",
+            sizes: "180x180",
+            type: "image/png",
+          },
+          {
+            src: "/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "/android-chrome-512x512.png", // 512px icon for splash screen
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+    }),
+  ],
+});
